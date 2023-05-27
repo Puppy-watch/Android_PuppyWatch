84aac621de8c4a37a2d3507f56547372a87c83db
yeji1008 <willy019@naver.com>
2023-05-27 20:22:46 +0900

    behavior server value success

--- app/src/main/java/com/example/puppywatch/BehaviorActivity.kt
+++ app/src/main/java/com/example/puppywatch/BehaviorActivity.kt
@@ -3,14 +3,17 @@ package com.example.puppywatch
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.puppywatch.databinding.ActivityBehaviorBinding
+import com.example.puppywatch.response.AbnormalResponse
+import com.example.puppywatch.view.AbnormalView
 
-class BehaviorActivity : AppCompatActivity() {
+class BehaviorActivity : AppCompatActivity(), AbnormalView {
 
     private lateinit var binding: ActivityBehaviorBinding
-    val itemList = arrayListOf<BehaviorListItem>()      // 아이템 배열
-    val listAdapter = BehavoirAdapter(itemList)     // 어댑터
+//    val itemList = arrayListOf<BehaviorListItem>()      // 아이템 배열
+//    val listAdapter = BehavoirAdapter(itemList)     // 어댑터
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,22 +34,42 @@ class BehaviorActivity : AppCompatActivity() {
 
         // 레이아웃 매니저와 어댑터 설정
         binding.behaviorRv.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-        binding.behaviorRv.adapter = listAdapter
+        binding.behaviorRv.setHasFixedSize(true)
+//        binding.behaviorRv.adapter = listAdapter
+//
+//        // 아이템 추가
+//        itemList.add(BehaviorListItem("2023.01.02 13:00:12 - 이상행동", "new"))
+//        itemList.add(BehaviorListItem("2023.01.12 13:00:12 - 이상행동", "new"))
+//        itemList.add(BehaviorListItem("2023.01.22 13:00:12 - 이상행동", "new"))
+//        itemList.add(BehaviorListItem("2023.01.32 13:00:12 - 이상행동", "new"))
+//        itemList.add(BehaviorListItem("2023.01.42 13:00:12 - 이상행동", "new"))
+//        itemList.add(BehaviorListItem("2023.01.52 13:00:12 - 이상행동", "new"))
+//        itemList.add(BehaviorListItem("2023.03.02 13:00:12 - 이상행동", "new"))
+//        itemList.add(BehaviorListItem("2023.03.12 13:00:12 - 이상행동", "new"))
+//        itemList.add(BehaviorListItem("2023.03.22 13:00:12 - 이상행동", "new"))
+//        itemList.add(BehaviorListItem("2023.03.32 13:00:12 - 이상행동", "new"))
+//        itemList.add(BehaviorListItem("2023.03.42 13:00:12 - 이상행동", "new"))
+//
+//        // 리스트가 변경됨을 어댑터에 알림
+//        listAdapter.notifyDataSetChanged()
 
-        // 아이템 추가
-        itemList.add(BehaviorListItem("2023.01.02 13:00:12 - 이상행동", "new"))
-        itemList.add(BehaviorListItem("2023.01.12 13:00:12 - 이상행동", "new"))
-        itemList.add(BehaviorListItem("2023.01.22 13:00:12 - 이상행동", "new"))
-        itemList.add(BehaviorListItem("2023.01.32 13:00:12 - 이상행동", "new"))
-        itemList.add(BehaviorListItem("2023.01.42 13:00:12 - 이상행동", "new"))
-        itemList.add(BehaviorListItem("2023.01.52 13:00:12 - 이상행동", "new"))
-        itemList.add(BehaviorListItem("2023.03.02 13:00:12 - 이상행동", "new"))
-        itemList.add(BehaviorListItem("2023.03.12 13:00:12 - 이상행동", "new"))
-        itemList.add(BehaviorListItem("2023.03.22 13:00:12 - 이상행동", "new"))
-        itemList.add(BehaviorListItem("2023.03.32 13:00:12 - 이상행동", "new"))
-        itemList.add(BehaviorListItem("2023.03.42 13:00:12 - 이상행동", "new"))
+        abnormal()
+    }
 
-        // 리스트가 변경됨을 어댑터에 알림
-        listAdapter.notifyDataSetChanged()
+    private fun abnormal() {
+        Log.d("abnormal()", "메소드")
+
+        val authService = AuthService()
+        authService.setAbnormalView(this)
+        authService.abnormal(1)
+    }
+
+    override fun onAbnormalSuccess(abnormalList: AbnormalResponse) {
+        Log.d("onAbnormalSuccess", "성공")
+        binding.behaviorRv.adapter = BehavoirAdapter(abnormalList.data)
+    }
+
+    override fun onAbnormalFailure() {
+        Log.d("onAbnormalFailure", "실패")
     }
 }
--- app/src/main/java/com/example/puppywatch/BehavoirAdapter.kt
+++ app/src/main/java/com/example/puppywatch/BehavoirAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.puppywatch
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,8 +9,11 @@ import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.example.puppywatch.response.AbListData
 
-class BehavoirAdapter(val itemList: ArrayList<BehaviorListItem>): RecyclerView.Adapter<BehavoirAdapter.ViewHolder>() {
+class BehavoirAdapter(val abnormalList: List<AbListData>): RecyclerView.Adapter<BehavoirAdapter.ViewHolder>() {
+    val TAG: String = "BehavoirAdapter"
+
     // 아이템 레이아웃과 결합
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BehavoirAdapter.ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.board_behavior_item, parent, false)
@@ -17,12 +21,17 @@ class BehavoirAdapter(val itemList: ArrayList<BehaviorListItem>): RecyclerView.A
     }
     // 리스트 내 아이템 개수
     override fun getItemCount(): Int {
-        return itemList.size
+        return abnormalList.size
     }
     // View에 내용 입력
     override fun onBindViewHolder(holder: BehavoirAdapter.ViewHolder, position: Int) {
-        holder.detail.text = itemList[position].detail
-        holder.new.text = itemList[position].new
+        Log.d(TAG, " - onBindViewHolder() called / position: $position")
+
+        if(abnormalList.size != 0) {
+            Log.d("date", abnormalList[position].abnormalTime)
+            Log.d("behavior", abnormalList[position].abnormalName)
+            holder.detail.text = abnormalList[position].abnormalTime + " " + abnormalList[position].abnormalName
+        }
 
         // CardView의 배경색 설정
         val cardView = holder.itemView.findViewById<CardView>(R.id.board_list_item_cv)
@@ -31,6 +40,5 @@ class BehavoirAdapter(val itemList: ArrayList<BehaviorListItem>): RecyclerView.A
     // 레이아웃 내 View 연결
     class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
         val detail: TextView = itemView.findViewById(R.id.board_behavior_item_tv)
-        val new: TextView = itemView.findViewById(R.id.board_behavior_item_new_tv)
     }
 }
