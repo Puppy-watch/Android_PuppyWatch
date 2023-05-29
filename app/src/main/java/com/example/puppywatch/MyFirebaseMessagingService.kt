43f6f689de0f18a740d31a673f529aa1e7666c01
suhyun21 <hyuuuun21@gmail.com>
2023-05-29 16:55:51 +0900

    notification 변경

--- app/src/main/java/com/example/puppywatch/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/puppywatch/MyFirebaseMessagingService.kt
@@ -26,11 +26,6 @@ class MyFirebaseMessagingService :  FirebaseMessagingService() {
         Log.d("fire test1", "firebase message received")
         schduleJob()
 
-        /*
-        if (remoteMessage.data.isNotEmpty()){
-            Log.d("fire test2",remoteMessage.data.toString())
-        }*/
-
         // 알림 확인
         remoteMessage.notification?.let { notification ->
             val title = notification.title // 알림 제목
@@ -70,6 +65,7 @@ class MyFirebaseMessagingService :  FirebaseMessagingService() {
             .setAutoCancel(true)
             .setSound(defaultSoundUri)
             .setContentIntent(pendingIntent)
+            .setSmallIcon(R.drawable.ic_cal_bite)
 
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
@@ -83,41 +79,6 @@ class MyFirebaseMessagingService :  FirebaseMessagingService() {
 
         notificationManager.notify(0 /* ID of notification */, notificationBuilder.build())
     }
-
-
-    /*
-
-    @RequiresApi(Build.VERSION_CODES.O)
-    private fun sendNotification(messageBody: String) {
-        val intent = Intent(this, MainActivity::class.java)
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
-
-        val channelId = getString(R.string.default_notification_channel_id)
-        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
-
-        val notificationBuilder = NotificationCompat.Builder(this, channelId)
-            .setContentTitle(messageBody.)
-            .setContentText(messageBody)
-            .setAutoCancel(true)
-            .setSound(defaultSoundUri)
-            .setContentIntent(pendingIntent)
-
-        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-
-        // Android 8.0 (Oreo) 이상에서는 알림 채널을 생성
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channel = NotificationChannel(
-                channelId,
-                getString(R.string.default_notification_channel_id),
-                NotificationManager.IMPORTANCE_DEFAULT
-            )
-            notificationManager.createNotificationChannel(channel)
-        }
-
-        notificationManager.notify(0, notificationBuilder.build())
-    }
-*/
     companion object {
         private const val TAG = "MyfirebaseService"
     }
